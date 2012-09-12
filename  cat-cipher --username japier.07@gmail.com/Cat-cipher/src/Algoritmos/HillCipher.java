64a2eab758e1e0835e8caf88a2c2e35393dda98f
japier.07@gmail.com <japier.07@gmail.com@2ec71900-5a1f-ca8a-f770-6377cd9f7f68>
2012-09-12 22:51:05 +0000

    HillCipher primer version funcionando :)

---  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
+++  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
@@ -1,5 +1,8 @@
 package Algoritmos;
 
+import Controller.Crypto;
+import Controller.Funciones;
+
 public class HillCipher {
 	static int[][] keyMatrix = new int[3][3];
 	public static String Encode(int[] key, String mensaje)
@@ -10,9 +13,43 @@ public class HillCipher {
 			keyMatrix[i/3][i%3]=key[j];
 			i++;
 			j++;
-			if(j==key.length)0
+			if(j==key.length)
 				j=0;
 		}
-		return null;
+		int tama = (mensaje.length())%3;
+		for(i=0; i<=tama; i++)
+		{
+			mensaje+="x";
+		}
+		int[] result = new int[mensaje.length()];
+		int[] mensa = Funciones.numerar(mensaje);
+		for(i=0; i<mensa.length-1; i+=3)
+		{
+			int[] dat = multiplicarMatrix(0, mensa[i], mensa[i+1], mensa[i+2]);
+			for(j=0; j<3; j++)
+				result[i+j]=dat[j];
+		}
+		return Funciones.parse(result);
+	}
+	public static int[] multiplicarMatrix(Object ... ob)
+	{
+		int[] ret=new int[3];
+		int index=(Integer)ob[0];
+		if(index==0)
+		{
+			for(int i=0; i<3; i++)
+				ret[i]=(((Integer)ob[1])*keyMatrix[i][0]+(Integer)ob[2]*keyMatrix[i][1]+(Integer)ob[3]*keyMatrix[i][2])%Crypto.AlfabetoNumber;
+		}
+		return ret;
+	}
+	public static int calcularDeterminante(Object ... ob)
+	{
+		int det=0;
+		int index = (Integer)ob[0];
+		if(index==0)
+		{
+			
+		}
+		return det;
 	}
 }
