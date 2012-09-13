d4a8f38f005fd260fde23d3564ebf79cfd1f1bab
japier.07@gmail.com <japier.07@gmail.com@2ec71900-5a1f-ca8a-f770-6377cd9f7f68>
2012-09-13 16:04:32 +0000

    hill cipher hasta matriz transpuesta

---  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
+++  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
@@ -1,14 +1,35 @@
 package Algoritmos;
 
+import java.io.ObjectInputStream.GetField;
+
 import Controller.Crypto;
 import Controller.Funciones;
 
 public class HillCipher {
 	static int[][] keyMatrix = new int[3][3];
+	static int[][] keyPrimaMatrix = new int[3][3];
 	public static String Encode(int[] key, String mensaje)
 	{
-		int i=0,j=0, h=-1;
-		int determinante=0;
+		int i=0,j=0;
+		calcularKey(key);
+		int tama = (mensaje.length())%3;
+		for(i=tama; i<3; i++)
+		{
+			mensaje+="x";
+		}
+		int[] result = new int[mensaje.length()];
+		int[] mensa = Funciones.numerar(mensaje);
+		for(i=0; i<mensa.length-1; i+=3)
+		{
+			int[] dat = multiplicarMatrix(0, mensa[i], mensa[i+1], mensa[i+2]);
+			for(j=0; j<3; j++)
+				result[i+j]=dat[j];
+		}
+		return Funciones.parse(result);
+	}
+	private static void calcularKey(int [] key)
+	{
+		int i=0,j=0,h=-1, determinante;
 		while(i!=9)
 		{
 			keyMatrix[i/3][i%3]=key[j];
@@ -29,20 +50,36 @@ public class HillCipher {
 			//h = (h+1)%3;
 			h++;
 		}while(determinante==0||determinante==2||determinante==13||determinante==26||determinante<0);
-		int tama = (mensaje.length())%3;
-		for(i=tama; i<3; i++)
+	}
+	public static String decode(int[] key, String mensaje)
+	{
+		getMatrixCofactores();
+		getTranspuesta();
+		return "";
+	}
+	private static void getTranspuesta()
+	{
+		int aux;
+		for(int i=0; i<2; i++)
+			for(int j=0; j<3; j++)
+			{
+				aux=keyPrimaMatrix[i][j];
+				keyPrimaMatrix[i][j]=keyPrimaMatrix[j][i];
+				keyPrimaMatrix[j][i]=aux;
+			}
+	}
+	private static void getMatrixCofactores()
+	{
+		int signo=1;
+		for(int i=0; i<3; i++)
 		{
-			mensaje+="x";
+			for(int j=0; j<3; j++)
+			{
+				keyPrimaMatrix[i][j]=signo* (keyMatrix[(i+1)%3][(j+1)%3]*keyMatrix[(i+2)%3][(j+2)%3]
+						-keyMatrix[(i+1)%3][(j+2)%3]*keyMatrix[(i+2)%3][(j+1)%3]);
+				signo*=-1;
+			}
 		}
-		int[] result = new int[mensaje.length()];
-		int[] mensa = Funciones.numerar(mensaje);
-		for(i=0; i<mensa.length-1; i+=3)
-		{
-			int[] dat = multiplicarMatrix(0, mensa[i], mensa[i+1], mensa[i+2]);
-			for(j=0; j<3; j++)
-				result[i+j]=dat[j];
-		}
-		return Funciones.parse(result);
 	}
 	public static int[] multiplicarMatrix(Object ... ob)
 	{
