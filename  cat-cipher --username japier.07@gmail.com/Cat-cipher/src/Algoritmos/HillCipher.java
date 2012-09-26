ba0fd2b27ec692fafe44fd908657dde468c1ed8d
blezka <blezka@2ec71900-5a1f-ca8a-f770-6377cd9f7f68>
2012-09-26 06:53:51 +0000

    corrigiendo matrices en hillcipher y modulo en cesar

---  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
+++  cat-cipher --username japier.07@gmail.com/Cat-cipher/src/Algoritmos/HillCipher.java
@@ -1,17 +1,20 @@
 package Algoritmos;
 
-import java.io.ObjectInputStream.GetField;
-
 import Controller.Crypto;
 import Controller.Funciones;
 
 public class HillCipher {
-	static int[][] keyMatrix = new int[3][3];
-	static int[][] keyPrimaMatrix = new int[3][3];
-	public static String Encode(int[] key, String mensaje)
+	static boolean debug = true;
+	
+	public static String code(int[] key, String mensaje)
+	{
+		int[][] keyMatrix = calcularKey(key);
+		return encode(keyMatrix,mensaje);
+	}
+	public static String encode(int[][] keyMatrix, String mensaje)
 	{
 		int i=0,j=0;
-		calcularKey(key);
+
 		int tama = (mensaje.length())%3;
 		for(i=tama; i<3; i++)
 		{
@@ -21,14 +24,15 @@ public class HillCipher {
 		int[] mensa = Funciones.numerar(mensaje);
 		for(i=0; i<mensa.length-1; i+=3)
 		{
-			int[] dat = multiplicarMatrix(0, mensa[i], mensa[i+1], mensa[i+2]);
+			int[] dat = multiplicarMatrix(keyMatrix,0, mensa[i], mensa[i+1], mensa[i+2]);
 			for(j=0; j<3; j++)
 				result[i+j]=dat[j];
 		}
 		return Funciones.parse(result);
 	}
-	private static void calcularKey(int [] key)
+	private static int[][] calcularKey(int[] key)
 	{
+		int[][] keyMatrix = new int[3][3];
 		int i=0,j=0,h=-1, determinante;
 		while(i!=9)
 		{
@@ -36,73 +40,126 @@ public class HillCipher {
 			i++;
 			j++;
 			if(j==key.length)
+			{
 				j=0;
+			}
 		}
+		if (debug)
+			for(int x=0; x< keyMatrix.length; x++)
+			{
+				for(int y=0; y < keyMatrix[x].length; y++)
+					System.out.print(keyMatrix[x][y]+",");
+				System.out.println();
+			}
 		do
 		{
 			if(h!=-1)
 			{
 				keyMatrix[(h/3)%3][h%3]=(keyMatrix[(h/3)%3][h%3]+1)%Crypto.AlfabetoNumber;
-//				keyMatrix[h][1]=(keyMatrix[h][1]+1)%Crypto.AlfabetoNumber;
-//				keyMatrix[h][2]=(keyMatrix[h][2]+1)%Crypto.AlfabetoNumber;
+				//				keyMatrix[h][1]=(keyMatrix[h][1]+1)%Crypto.AlfabetoNumber;
+				//				keyMatrix[h][2]=(keyMatrix[h][2]+1)%Crypto.AlfabetoNumber;
 			}
-			determinante=calcularDeterminante(0);
+			determinante=Funciones.getModulo(calcularDeterminante(keyMatrix));
 			//h = (h+1)%3;
 			h++;
 		}while(determinante==0||determinante==2||determinante==13||determinante==26||determinante<0);
+		return keyMatrix;
 	}
 	public static String decode(int[] key, String mensaje)
 	{
-		getMatrixCofactores();
-		getTranspuesta();
-		return "";
+		int[][] keyMatrix = new int[3][3];
+		keyMatrix=calcularKey(key);	
+		int x = findX(keyMatrix);
+		keyMatrix=getMatrixCofactores(keyMatrix);
+		keyMatrix=getTranspuesta(keyMatrix);
+		
+		for(int i = 0; i < keyMatrix.length; i++)
+			for(int j = 0; j < keyMatrix.length;j++)
+				keyMatrix[i][j]= Funciones.getModulo(keyMatrix[i][j]*x);
+		
+		return encode(keyMatrix,mensaje);
 	}
-	private static void getTranspuesta()
+	private static int findX(int[][] key)
 	{
-		int aux;
-		for(int i=0; i<2; i++)
-			for(int j=0; j<3; j++)
+		int det = Funciones.getModulo(calcularDeterminante(key));
+		for(int i = 0; i < Crypto.AlfabetoNumber;i++)
+		{
+			double x = i*Crypto.AlfabetoNumber+1;
+			if(x%det==0)
+				return (i*Crypto.AlfabetoNumber+1)/det;
+		}
+		return -1;
+	}
+	private static int[][] getTranspuesta(int[][] keyPrimaMatrix)
+	{
+		for(int i=0; i<keyPrimaMatrix.length; i++)
+			for(int j=0; j<keyPrimaMatrix.length; j++)
 			{
-				aux=keyPrimaMatrix[i][j];
+				int aux=keyPrimaMatrix[i][j];
 				keyPrimaMatrix[i][j]=keyPrimaMatrix[j][i];
 				keyPrimaMatrix[j][i]=aux;
 			}
+		return keyPrimaMatrix;
 	}
-	private static void getMatrixCofactores()
+	private static int[][] getMatrixCofactores(int[][] keyMatrix)
 	{
 		int signo=1;
-		for(int i=0; i<3; i++)
+		int max = keyMatrix.length;
+		int[][] key = new int[max][max];
+		for(int i=0; i<max; i++)
 		{
-			for(int j=0; j<3; j++)
+			for(int j=0; j<max; j++)
 			{
-				keyPrimaMatrix[i][j]=signo* (keyMatrix[(i+1)%3][(j+1)%3]*keyMatrix[(i+2)%3][(j+2)%3]
-						-keyMatrix[(i+1)%3][(j+2)%3]*keyMatrix[(i+2)%3][(j+1)%3]);
+				int[][] keyPrimaMatrix = new int[max-1][max-1];
+				for(int ip = 0; ip < max-1 ; ip++)
+						for(int jp=0; jp < max-1 ; jp++)
+							keyPrimaMatrix[ip][jp]=keyMatrix[ip>=i?(ip+1)%max:ip][jp>=j?(jp+1)%max:jp];
+				key[i][j] = signo*calcularDeterminante(keyPrimaMatrix);
 				signo*=-1;
 			}
 		}
+		return key;
 	}
-	public static int[] multiplicarMatrix(Object ... ob)
+	public static int[] multiplicarMatrix(int[][] keyMatrix,int... ob)
 	{
 		int[] ret=new int[3];
 		int index=(Integer)ob[0];
 		if(index==0)
 		{
 			for(int i=0; i<3; i++)
-				ret[i]=(((Integer)ob[1])*keyMatrix[0][i]+(Integer)ob[2]*keyMatrix[1][i]+(Integer)ob[3]*keyMatrix[2][i])%Crypto.AlfabetoNumber;
+			{
+				ret[i]=Funciones.getModulo(ob[1]*keyMatrix[i][0]+ob[2]*keyMatrix[i][1]+ob[3]*keyMatrix[i][2]);
+				if(debug)
+					System.out.print(ret[i]+",");
+			}
 		}
+		if(debug)
+			System.out.println();
 		return ret;
 	}
-	public static int calcularDeterminante(Object ... ob)
+	public static int calcularDeterminante(int[][] keyMatrix)
 	{
 		int det=0;
-		int index = (Integer)ob[0];
-		if(index==0)
-		{
-			det = (keyMatrix[0][0]*(keyMatrix[1][1]*keyMatrix[2][2]-keyMatrix[2][1]*keyMatrix[1][2])-
-					keyMatrix[0][1]*(keyMatrix[0][1]*keyMatrix[2][2]-keyMatrix[0][2]*keyMatrix[2][1])+
-					keyMatrix[0][2]*(keyMatrix[0][1]*keyMatrix[1][2]-keyMatrix[0][2]*keyMatrix[1][1]));
-			det = Funciones.getModulo(det);
+		int max = keyMatrix.length;
+			for(int i = 0; i <max-1; i++)
+			{
+				int pos = 1;
+				int neg = 1;
+				for(int j=0; j < max; j++)
+				{
+					int x = j;
+					int y = (j+i)%max;
+					if(debug)
+					System.out.println("+"+pos+"*"+keyMatrix[x][y]);
+					pos *= keyMatrix[x][y];
+					if(debug)
+					System.out.println("-"+neg+"*"+keyMatrix[x][max-1-y]);
+					neg *= keyMatrix[x][max-1-y];
+				}
+				det += pos-neg;
 		}
+		if(debug)
+			System.out.println("determinante = "+det);
 		return det;
 	}
 }
